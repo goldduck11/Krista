870c7ff5d7a8a618a63ef635cf690c1b94a6c337
goldduck11 <anderson229411@mail.ru>
2023-11-22 00:59:53 +0300

    init

--- lab1/src/main/java/providers/MongoDBProvider.java
+++ lab1/src/main/java/providers/MongoDBProvider.java
@@ -7,17 +7,26 @@ import com.google.gson.Gson;
 import com.mongodb.*;
 import com.mongodb.MongoClient;
 import com.mongodb.client.*;
+import com.mongodb.client.result.UpdateResult;
 import com.mongodb.util.JSON;
 import models.News;
 import org.bson.Document;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.ogm.jpa.HibernateOgmPersistence;
 import org.hibernate.query.Query;
+import org.hibernate.sql.Update;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.print.Doc;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.mongodb.client.model.Filters.where;
 
 public class MongoDBProvider implements SQLProvider {
 
@@ -25,65 +34,38 @@ public class MongoDBProvider implements SQLProvider {
 
     @Override
     public List<News> getAllNews() {
-        try (MongoClient mongodb = new MongoClient(this.URI)) {
-            DB db = mongodb.getDB("customData-database");
-            DBCursor customDataCollection = db.getCollection("customDataList").find();
-            List<News> myCustomDataList; // this list will hold your custom data
-            ObjectMapper objectMapper = new ObjectMapper();
-            myCustomDataList = objectMapper.readValue(JSON.serialize(customDataCollection),
-                    new TypeReference<List<News>>() {
-                    });
-           return myCustomDataList;
-        } catch (MongoException | JsonProcessingException me) {
-            System.err.println(me);
+        try (Session session = HibernateMo){
+
         }
-        return null;
     }
 
     @Override
     public void update(News news) {
-        try (MongoClient mongodb = new MongoClient(this.URI)) {
-            MongoDatabase db = mongodb.getDatabase(NameDB.LOCAL);
-            MongoCollection collection = db.getCollection("startup_log");
-            Gson gson = new Gson();
-            String json = gson.toJson(news);
-            BasicDBObject query = new BasicDBObject();
-            query.put("suitename", json);
-            BasicDBObject push = new BasicDBObject();
-            collection.updateOne(query, push);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mangodb");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.
+        entityManager.close();
+        entityManagerFactory.close();
     }
 
     @Override
     public void delete(News news) {
-        try (MongoClient mongodb = new MongoClient(this.URI)) {
-            MongoDatabase db = mongodb.getDatabase(NameDB.LOCAL);
-            MongoCollection collection = db.getCollection("startup_log");
-            Gson gson = new Gson();
-            String json = gson.toJson(news);
-            BasicDBObject basicDBObject = new BasicDBObject("Name", json );
-            BasicDBObject document = new BasicDBObject(basicDBObject);
-            collection.deleteMany(document);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mangodb");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.remove(news);
+        entityManager.close();
+        entityManagerFactory.close();
+
     }
 
     @Override
     public void add(News news) {
-        try (MongoClient mongodb = new MongoClient(this.URI)) {
-            mongodb.getDatabase(NameDB.LOCAL);
-            DB db = mongodb.getDB("customData-database");
-            Gson gson = new Gson();
-            String json = gson.toJson(news);
-            BasicDBObject basicDBObject = new BasicDBObject("Name", json );
-            DBCollection dbCollection = db.getCollection("NameColl");
-            dbCollection.save(basicDBObject);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mangodb");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(news);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
